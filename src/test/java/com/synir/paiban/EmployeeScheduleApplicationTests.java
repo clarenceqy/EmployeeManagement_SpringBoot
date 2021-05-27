7723c05d5e928fa5b1ea42158bf082ed86aa0ee5
Yichao Qin <31835513+clarenceqy@users.noreply.github.com>
2021-05-27 14:49:32 +0800

    Add files via upload

---
+++ src/test/java/com/synir/paiban/EmployeeScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synir.paiban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
